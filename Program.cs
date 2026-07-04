19307d45ff93c5727d3321e85c2dd09b90f4bf36
asmarjb <asmarjb@code.edu.az>
2026-07-04 05:34:04 +0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using BookStore.Infrastructureprotected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+{
+    optionsBuilder.UseSqlServer("Server=.\\SqlExpress;Database=AcademyDbNthier402;Trusted_Connection=True;TrustServerCertificate=True");
+};
+
+namespace BookStoreManagementSystem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var context = new AppDbContext();
+        }
+    }
+}
